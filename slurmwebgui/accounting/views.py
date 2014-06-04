a04b3894cde624fb9a8f7e6538a5e25fa4b853c3
Cristóbal Leiva Aburto <cristobal.leiva@usach.cl>
2014-06-04 15:57:56 -0300

    Test form for adding QoS based in the QOS Form Class

--- slurmwebgui/accounting/views.py
+++ slurmwebgui/accounting/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import RequestContext, loader
+from accounting.forms import QOS
 
-# Create your views here.
+# Form for adding a QoS
+def index(request):
+	template = loader.get_template('forms/qos.html')
+	form = QOS()
+	context = RequestContext(request, {'form': form,})
+	return HttpResponse(template.render(context))
--- slurmwebgui/slurmwebgui/settings.py
+++ slurmwebgui/slurmwebgui/settings.py
@@ -36,6 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounting',
+    'bootstrap3',
 )
 
 MIDDLEWARE_CLASSES = (
